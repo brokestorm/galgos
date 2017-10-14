e3557616ab9aa3573b2e15c5b4cb73d536781b34
Raphael Bandeira <raphael.bandeira.ismart@gmail.com>
2017-10-14 00:10:19 -0300

    Changed the batch file function to move files
    now it wont open the directory, it will instead move the files from the save directory to actual path

--- HDG/HD_Generator.bat
+++ HDG/HD_Generator.bat
@@ -1,3 +1,6 @@
 love.exe Source
-cd "%APPDATA%/LOVE/HD_Generator/HardData"
-start .
+xcopy %APPDATA%\LOVE\HD_Generator /s /y
+cd /d c:
+cd %APPDATA%\LOVE\HD_Generator\HardData
+del %APPDATA%\LOVE\HD_Generator\HardData /s /q
+Rd %APPDATA%\LOVE\HD_Generator\HardData /s /q
